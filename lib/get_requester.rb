df279d507c92cfdf87b5b81080d49aa89a90db56
Joshua Williams <jmw0426@hotmail.com>
2020-06-16 16:29:47 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,5 +1,7 @@
 class GetRequester
   
+  URL = 
+  
   def get_response_body
     
   end 
@@ -8,7 +10,24 @@ class GetRequester
     
   end 
   
-  
+  require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetPrograms
+
+  URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+
+  def get_programs
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+end
+
+programs = GetPrograms.new.get_programs
+puts programs
   
   
 end
